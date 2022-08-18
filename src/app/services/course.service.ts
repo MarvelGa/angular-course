8cd3b7926e6aedac4dc09206a0dcafaf017eefe9
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-08-18 13:58:54 +0200

    added service for course with methods

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class CourseService{
+  constructor() {
+  }
+
+  getList(){
+
+  }
+
+  createCourse(){
+
+  }
+
+  getItemById(){
+
+  }
+
+  updateItem(){
+
+  }
+
+  removeItem(){
+
+  }
+}
