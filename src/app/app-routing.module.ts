9f31a87d2b59dc9156bc09093e34fa20d3a3468c
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-08-31 01:03:40 +0200

    added PageNotFound component and module, implemented 404 error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
-import {AuthGuard} from "./shared/auth-guard/auth.guard";
+import { AuthGuard } from "./shared/auth-guard/auth.guard";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', loadChildren: () => import ('./pages/login/login.module').then(m => m.LoginModule)},
-  {path: 'courses', loadChildren: () => import ('./pages/courses/courses.module').then(m => m.CoursesModule), canLoad: [AuthGuard]}
+  {path: 'courses', loadChildren: () => import ('./pages/courses/courses.module').then(m => m.CoursesModule), canLoad: [AuthGuard]},
+  {path: '**', component:PageNotFoundComponent}
 ];
 
 @NgModule({
