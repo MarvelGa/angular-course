e7f5e02475361356b5cad785536e7195e75b3c44
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-09-01 13:43:18 +0200

    added one more test and edited unit test in MessageService

--- src/app/shared/services/message.service.spec.ts
+++ src/app/shared/services/message.service.spec.ts
@@ -2,10 +2,18 @@ import {MessageService} from "./message.service";
 
 describe('(MessageService)', () => {
 
-  it('should change message', () => {
+  it('should invoke the "message" function', () => {
+    const messageService = new MessageService();
+    spyOn(messageService, "changeMessage");
+    const message = 'transferring message';
+    messageService.changeMessage(message);
+    expect(messageService.changeMessage).toHaveBeenCalled()
+  });
+
+  it('should transfer the "message"', () => {
     const messageService = new MessageService();
     const message = 'transferring message';
     messageService.changeMessage(message);
-    expect(messageService.changeMessage(message)).withContext(message);
+    expect(messageService.currentMessage).withContext(message)
   });
 });
