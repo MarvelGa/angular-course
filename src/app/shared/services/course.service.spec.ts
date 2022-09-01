ace540cfe3aab32bff507c04e0dec63de14fd127
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-09-01 12:07:29 +0200

    added unit tests to CourseService, added codeCoverage to angular.json

---
+++ src/app/shared/services/course.service.spec.ts
@@ -0,0 +1,58 @@
+import {CourseService} from "./course.service";
+import {CourseInterface} from "../../types/course.interface";
+
+describe('(CourseService)', () => {
+
+  it('should get list of courses', () => {
+    const courseService = new CourseService();
+    const result = courseService.getList();
+    expect(result.length).toBe(4)
+  });
+
+  it('should add a new course to the list of courses', () => {
+    let newCourse = {} as CourseInterface;
+    newCourse = {
+      id: '5',
+      title: 'Test course',
+      creationDate: 1668898800000,
+      duration: '90',
+      description: 'Test — один из наиболее распространенных языков программирования в мире.',
+      topRated: false
+    }
+    const courseService = new CourseService();
+    const result = courseService.createCourse(newCourse);
+    expect(result.length).toBe(5)
+    expect(result[4]).toEqual(newCourse)
+  });
+
+  it('should get course by id', () => {
+    const courseService = new CourseService();
+    const result = courseService.getItemById('1');
+    expect(result.id).toEqual('1')
+  });
+
+  it('should update course', () => {
+    const courseService = new CourseService();
+    let updatedCourse = {} as CourseInterface;
+    let courseUpdateId = '1';
+    updatedCourse = {
+      id: courseUpdateId,
+      title: 'Updated course',
+      creationDate: 1668898800000,
+      duration: '90',
+      description: 'Updated',
+      topRated: false
+    }
+    courseService.updateItem(updatedCourse);
+    const updated = courseService.getItemById(courseUpdateId)
+    expect(updated).toEqual(updatedCourse);
+  });
+
+  it('should remove course by id', () => {
+    const courseService = new CourseService();
+    let courseId = '1';
+    let quantity = courseService.getList().length;
+    const result = courseService.removeItem(courseId);
+    expect(result.length).toEqual(quantity - 1)
+  });
+});
