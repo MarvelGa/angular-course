0a8594eee8a851c8776be47a7aff46fdc7f46f13
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-09-04 18:19:00 +0200

    added unit tests for HeaderComponent

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import {Router} from "@angular/router";
+import {AuthService} from "../../shared/services/auth.service";
+import {FormsModule} from "@angular/forms";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
+  let service: AuthService;
   beforeEach(async () => {
+
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [FormsModule],
+      declarations: [ HeaderComponent ],
+      providers:[
+        {provide: Router, useValue: routerSpy},
+        {provide: AuthService,  useValue:{ getUserInfo: ()=> (of([])) , isAuthenticated: ()=> of (true), logout: ()=> of(true), currentUserEmail: ()=> of('test@gmail.com') }}
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(AuthService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should invoke "logout" method of AuthService and navigate to "login" page' , () => {
+    component.logout();
+    let router = fixture.debugElement.injector.get(Router);
+    const spy = router.navigate as jasmine.Spy;
+    const navArgs = spy.calls.first().args[0];
+    expect(navArgs).withContext('should navigate by the url')
+      .toEqual([ 'login' ]);
+  });
+
+  it ('should get user info', ()=>{
+    component.userEmail='test@gmail.com';
+    let user = {
+      id: Date.now().toString(), email: 'test@gmail.com', password: 'password', token: 'JWT_TOKEN'
+    };
+    spyOn(service, 'getUserInfo').and.returnValue([user]);
+    localStorage.setItem('email', 'test@gmail.com');
+    let result = component.getUserInfo();
+    expect(result).toEqual('test@gmail.com');
+    localStorage.removeItem('email');
+
+  });
+
 });
